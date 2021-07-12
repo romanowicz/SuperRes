df6a2b8579bbc95fed6f537b8b31be319a4f961e
Romanowicz <romanowicz@protonmail.ch>
2021-07-12 09:44:25 +0200

    add neural network model

--- train.py
+++ train.py
@@ -6,13 +6,15 @@ Created on Sun Jul 11 22:06:56 2021
 @author: rpa
 """
 
-#import torch
+import torch
 import os
 import glob
 
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 from torch.utils.data import DataLoader
+from torch import nn
+
 #from torchvision import datasets
 #from torchvision.transforms import ToTensor
 
@@ -43,9 +45,33 @@ class CustomImageDataset(Dataset):
         if self.transform:
             image_l = self.transform(image_l)
         if self.target_transform:
-            image_h = self.target_transform(label_h)
+            image_h = self.target_transform(image_h)
         return image_l, image_h
 
+
+class SRCNN(nn.Module):
+    def __init__(self):
+        super(SRCNN, self).__init__()
+        c = 3
+        f1 = 9
+        f2 = 1
+        f3 = 5
+        n1 = 64
+        n2 = 32
+        self.conv1 = torch.nn.Conv2d(c, n1, kernel_size=f1, stride=1)
+        self.relu1 = torch.nn.ReLU()
+        self.conv2 = torch.nn.Conv2d(n1, n2, kernel_size=f2, stride=1)
+        self.relu2 = torch.nn.ReLU()
+        self.conv3 = torch.nn.Conv2d(n2, c, kernel_size=f3, stride=1)
+
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.relu1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.conv3(y)        
+        return y
+    
 
 # make dataset and loader
 dataset = CustomImageDataset("", DATA_PATH)
@@ -54,8 +80,13 @@ train_dataloader = DataLoader(dataset, batch_size=batch_size)
 # print info
 for X, Y in train_dataloader:
     print("Shape of X [N, C, H, W]: ", X.shape, X.dtype)
-    print("Shape of Y [N, C, H, W]: ", y.shape, y.dtype)
+    print("Shape of Y [N, C, H, W]: ", Y.shape, Y.dtype)
     break
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using {} device'.format(device))
+
+model = SRCNN().to(device)
+print(model)
 
 
