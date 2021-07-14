058307fc37a693077d086d1b2383c233e2649728
Romanowicz <romanowicz@protonmail.ch>
2021-07-14 15:14:01 +0200

    add model, image, dataset update

--- SRCNNDataset.py
+++ SRCNNDataset.py
@@ -41,17 +41,19 @@ class SRCNNDataset(Dataset):
         return num
 
 
-    def __init__(self, annotations_file, img_dir, max_images=None, transform=None, target_transform=None):        
+    def __init__(self, annotations_file, img_dir, max_images=None, tile_size=TILE_SIZE, stride=STRIDE, transform=None, target_transform=None):        
         self.file_list = glob.glob(img_dir + os.sep + "*.jpg")
         if max_images is not None and max_images > 0:
             m = min(max_images, len(self.file_list))
             self.file_list = self.file_list[:m]
 
         self.img_dir = img_dir
-        self.tiles_x = self.tilesPerImage(IMG_SIZE_X, TILE_SIZE, STRIDE)
-        self.tiles_y = self.tilesPerImage(IMG_SIZE_Y, TILE_SIZE, STRIDE)
+        self.tiles_x = self.tilesPerImage(IMG_SIZE_X, tile_size, stride)
+        self.tiles_y = self.tilesPerImage(IMG_SIZE_Y, tile_size, stride)
         self.num_tiles_per_img = self.tiles_x * self.tiles_y
         self.num_tiles = self.num_tiles_per_img * len(self.file_list)
+        self.tile_size=tile_size
+        self.stride=stride
         
         print("Dataset info")
         print("num_images    : " + str(len(self.file_list)))
@@ -89,9 +91,9 @@ class SRCNNDataset(Dataset):
         tile_idx = idx - self.img_idx * self.num_tiles_per_img
         tile_idx_x = int(tile_idx % self.tiles_x)
         tile_idx_y = int(tile_idx / self.tiles_x)                
-        tile_offs_x = tile_idx_x * STRIDE
-        tile_offs_y = tile_idx_y * STRIDE
-        bbox = (tile_offs_x, tile_offs_y, tile_offs_x + TILE_SIZE, tile_offs_y + TILE_SIZE)
+        tile_offs_x = tile_idx_x * self.stride
+        tile_offs_y = tile_idx_y * self.stride
+        bbox = (tile_offs_x, tile_offs_y, tile_offs_x + TILE_SIZE, tile_offs_y + self.tile_size)
         tile_l = self.img_l.crop(bbox)
         tile_h = self.img_h.crop(bbox)
         
